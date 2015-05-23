dc9877f033b53692d3be8635d4a2bfbf4a694f2a
awesometrainexpress <robert.warnes@my.jcu.edu.au>
2015-05-23 13:26:35 +1000

    Started PHP Cleanup

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,5 @@
 <?php
 try {
-/* connect to SQLite database. It's good to have this in a separate file you can include in all pages that need DB access */
     $dbh = new PDO("sqlite:tcmcdatabase.sqlite"); 
 }
 catch(PDOException $e)
--- musicians.php
+++ musicians.php
@@ -1,17 +1,48 @@
 <?php
 include("dbconnect.php");
 ?>
-<!DOCTYPE html>
-<html lang="">
+<!doctype html>
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-    <link rel="stylesheet" href="stylesheet.css">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="main.css" rel="stylesheet" type="text/css">
+<title>Artists - TCMC</title>
 </head>
-
+<header>
+<div id="header">
+<div id="header01">
+<a href="index.html"><img src="images/tcmclogo.jpg" alt=""></a>
+</div>
+<div id="header02">
+<i> - “Promoting Local Music Culture In The Townsville Area” - </i>
+<div id="header03">
+Want Exclusive Discounts?
+<br>
+<br>
+<img src="images/memberbutton.jpg" alt=""> 
+</div>
+    
+<div id="header04">
+<img src="images/signinbutton.jpg" alt="">
+</div>
+    
+<div id="header05">
+<a href="https://www.facebook.com/pages/Townsville-Community-Music-Centre/159636880763534"><img src="images/fblogo.jpg" alt=""></a>
+</div>
+</div>
+</header>
 <body>
-    <h1>Musicians Database</h1>
+<img src="images/artistsbanner.jpg" width="75%" alt="" img style="margin:0px auto;display:block">
+<div id="bodywrapper">
+<ul id="breadcrumbs">
+<il><a id="breadcrumbs" href="index.html">HOME</a></il>
+<il> > </il>
+<il><a id="breadcrumbs" href="artists.html">ABOUT US</a></il>
+<il> > </il>
+<il><a id="breadcrumbs" href="artists.html">ABOUT US</a></il>
+</ul>
+    <h1>Register Your Act!!!</h1>
     
     <form id="insert" name="insert" method="post" action="dbprocessmusician.php" enctype="multipart/form-data">
     <input type="hidden" name="MAX_FILE_SIZE" value="4194304" />
@@ -72,8 +103,53 @@ foreach ($dbh->query($sql) as $row)
 <?php
 }
 echo "</fieldset>\n";
-// close the database connection
 $dbh = null;
 ?>
 </body>
+<footer>
+<div id= "footer">
+<div id="footercontainer">
+<div id="footerlinks01">
+<a href="artists.html"><b>ARTISTS</b></a>
+<hr>
+<a href="">SEARCH</a>
+<a href="">LATEST</a>
+<a href="">GENRES</a>
+<a href="">NAMES A-Z</a></p>
+</div>
+<div id="footerlinks02">
+<a href="events.html"><b>EVENTS</b></a>
+<hr>
+<a href="">ALL</a>
+<a href="">LATEST</a>
+</div>
+<div id="footerlinks03">
+<a href="bulletin"><b>BULLETIN</b></a>
+<hr>
+<a href="">JOB OFFERS</a>
+<a href="">TUTORING</a>
+<a href="">EXPRESSION</a>
+</div>
+<div id="footerlinks04">
+<a href="sponsors.html"><b>SPONSORS</b></a>
+<hr>
+<a href="sponsors.html">LIST</a>
+</div>
+<div id="footerlinks05">
+<a href="contactus.html"><b>CONTACT US</b></a>
+<hr>
+<a href="">E-MAIL</a>
+<a href="">PHONE</a>
+<a href="">POSTAL</a>
+<a href="">ADDRESS</a>
+</div>
+<div id="footerlinks06">
+<a href=""><b>ABOUT US</b></a>
+<hr>
+<a href="">HISTORY</a>
+<a href="">TIMELINE</a>
+</div>
+</div>
+</div>
+</footer>
 </html>
